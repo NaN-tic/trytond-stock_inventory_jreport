a2ecf2d3316ede49b01767750ee76833764d44c9
Raimon Esteve <raimon@nan-tic.com>
2017-09-25 17:46:19 +0200

    Show report title according locations and stock date end

--- inventory.py
+++ inventory.py
@@ -1,9 +1,10 @@
-# This file is part of stock_inventory_jasper module for Tryton.
+# This file is part of stock_inventory_jreport module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+import datetime
 from trytond.modules.jasper_reports.jasper import JasperReport
 from trytond.transaction import Transaction
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 
 
 __all__ = ['InventoryReport', 'BlindCountReport', 'InventoryValuedReport']
@@ -23,10 +24,24 @@ class InventoryValuedReport(JasperReport):
 
     @classmethod
     def execute(cls, ids, data):
+        Location = Pool().get('stock.location')
+
+        context = Transaction().context
+        stock_date_end = context.get('stock_date_end', datetime.date.today())
+        locale = context.get('locale')
+        if locale:
+            dformat = locale['date']
+        else:
+            dformat = '%Y/%m%d/'
+
+        if 'parameters' not in data:
+            data['parameters'] = {}
+        data['parameters']['stock_date_end'] = stock_date_end.strftime(dformat)
         if 'locations' in data:
-            if 'parameters' not in data:
-                data['parameters'] = {}
             data['parameters']['locations'] = data['locations']
+        elif context.get('locations'):
+            data['parameters']['locations'] = ' / '.join([
+                l.report_title() for l in Location.browse(context.get('locations'))])
         if 'context' in data:
             with Transaction().set_context(data['context']):
                 return super(InventoryValuedReport, cls).execute(ids, data)
