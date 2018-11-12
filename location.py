280a3e3d1e7878a2ce3b5dfa1af57554909b5a04
Raimon Esteve <raimon@nan-tic.com>
2018-11-12 15:15:10 +0100

    Add inventory_cost_value field #034631

--- location.py
+++ location.py
@@ -1,9 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import datetime
-from trytond.wizard import StateAction, Button
-from trytond.transaction import Transaction
-from trytond.pool import PoolMeta, Pool
+from trytond.pool import PoolMeta
 
 __all__ = ['Location']
 
