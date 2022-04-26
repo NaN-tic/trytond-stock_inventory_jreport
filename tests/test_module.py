bdb54a485dbaf624285ed0ca8252d40dede5ffaa
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:21:25 +0200

    Replace test setuptools command by unittest discover

---
+++ tests/test_module.py
@@ -0,0 +1,14 @@
+
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.modules.company.tests import CompanyTestMixin
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class StockInventoryJreportTestCase(CompanyTestMixin, ModuleTestCase):
+    'Test StockInventoryJreport module'
+    module = 'stock_inventory_jreport'
+
+
+del ModuleTestCase
